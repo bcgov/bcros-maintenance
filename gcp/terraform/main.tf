31cd1371d99c9c9d6207b6857b7b774cf1c577f3
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2025-03-04 22:21:26 -0800

    workload identity experiment

--- gcp/terraform/main.tf
+++ gcp/terraform/main.tf
@@ -10,7 +10,21 @@ terraform {
 provider "google" {
   project = null
   region  = var.region
-  credentials = var.GOOGLE_CREDENTIALS
+}
+
+variable "TFC_GCP_PROVIDER_AUTH" {
+  description = "Terraform Cloud will use dynamic credentials to authenticate to GCP"
+  type        = string
+}
+
+variable "TFC_GCP_RUN_SERVICE_ACCOUNT_EMAIL" {
+  description = "The service account email address that Terraform Cloud will use to authenticate to Google Cloud"
+  type        = string
+}
+
+variable "TFC_GCP_WORKLOAD_PROVIDER_NAME" {
+  description = "The canonical name of the workload identity provider"
+  type        = string
 }
 
 locals {
